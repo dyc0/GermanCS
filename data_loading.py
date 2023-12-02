cb4bfbcfe3af1d5889fbde1a2f9e906c1a35b1ba
dyc0 <dusancvijetic2000@gmail.com>
2023-12-02 22:00:24 +0100

    Word loading modified, now info about grammatical and semantical errors is loaded, too.

--- data_loading.py
+++ data_loading.py
@@ -5,8 +5,7 @@ def create_word_lists(data: list) -> tuple(list, list):
     """Function to extract word lists for STT and human transcripts.
     Removes all @ annotations from human transcript and classifies the output
     according to whether the utterance is in German or English, where German is
-    denoted as 1 and English as 0. If either STT or human transcript contains an
-    empty string at a certain position, it is skipped.
+    denoted as 1 and English as 0.
 
     Args:
         data (list): list of dictionaries, each containing 'words' key that points
@@ -14,49 +13,39 @@ def create_word_lists(data: list) -> tuple(list, list):
             [{'words': {'human_word': humw, 'stt_word': sttw}, ... }, ...]
 
     Returns:
-        tuple(list, list, list): Tuple containing a list of lists of words from human transcripts,
-            a list of lists of words from STT transcripts and a list of lists of classes (E or G).
+        tuple(list, list, list, lsit, list): Tuple containing five lists: human word
+            transcripts, STT word transcripts, language labels, grammatical error
+            indicators and semantical error indicators.
     """
 
-    human_words = []
-    stt_words = []
-    word_classes = []
+    human_words = []  # human-transcribed words
+    stt_words = []  # STT transcribed words
+    word_labels = []  # language labels
+    word_sems = []  # semantical errors
+    word_grams = []  # grammatical errors
 
     for entry in data:
         entry_stt_words = []
         entry_hum_words = []
-        entry_classes = []
+        entry_labels = []
+        entry_grams = []
+        entry_sems = []
 
         for word in entry["words"]:
             humw = word["human_word"]
             sttw = word["stt_word"]
 
-            # if either stt or human transcript don't contain a word, skip it
-            # TODO: we might want to split the phrases if we use this for features
-            if re.sub(r"@.", "", humw) == "" or sttw == "":
-                continue
-
-            # if it is a german word/phrase, annotate it
-            if "@g" in humw:
-                entry_hum_words.append(re.sub(r"@.", "", humw))
-                entry_classes.append(1)
-
-            # if the word contains another type of a mistake, just remove annotations - we don't care
-            # TODO: we can also add different markings for each type of mistake for the learning algorithm later
-            elif "@" in humw:
-                entry_hum_words.append(re.sub(r"@.", "", humw))
-                entry_classes.append(0)
-
-            # otherwise, just add the word
-            else:
+            for w in sttw.split():
+                entry_stt_words.append(w)
                 entry_hum_words.append(humw)
-                entry_classes.append(0)
-
-            # stt words don't have to be cleaned
-            entry_stt_words.append(word["stt_word"])
+                entry_labels.append("@g" in humw)
+                entry_grams.append("@!" in humw)
+                entry_sems.append("@?" in humw)
 
         human_words.append(entry_hum_words)
         stt_words.append(entry_stt_words)
-        word_classes.append(entry_classes)
+        word_labels.append(entry_labels)
+        word_grams.append(entry_grams)
+        word_sems.append(entry_sems)
 
-    return human_words, stt_words, word_classes
+    return human_words, stt_words, word_labels, word_grams, word_sems
